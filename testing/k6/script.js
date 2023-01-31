55c3aa31fca3f2961fb0ad2bc9812dc6606f9056
mindiweik <mindiweik@gmail.com>
2023-01-30 19:56:56 -0700

    added put requests and started post request additions

--- testing/k6/script.js
+++ testing/k6/script.js
@@ -2,7 +2,7 @@ import http from 'k6/http'
 import { sleep } from 'k6'
 
 export const options = {
-  vus: 1,
+  vus: 10,
   duration: '60s',
 }
 
@@ -12,8 +12,34 @@ export default function () {
   http.get('http://localhost:3030/qa/questions/88/answers')
 
   // PUT requests
-  http.get('http://localhost:3030/qa/questions/75/helpful')
-  http.get('http://localhost:3030/qa/answers/148/helpful')
+  http.put('http://localhost:3030/qa/questions/75/helpful')
+  http.put('http://localhost:3030/qa/answers/148/helpful')
+  http.put('http://localhost:3030/qa/questions/45/report')
+  http.put('http://localhost:3030/qa/answers/100/report')
+
+  // POST requests
+  const questionPost = JSON.stringify({
+    product_id: '55',
+    body: 'K6 test body',
+    name: 'I test K6',
+    email: 'k6@test.com'
+  })
+
+  const answerPost = JSON.stringify({
+    body: 'K6 test answer body',
+    name: 'I test K6',
+    email: 'k6@test.com',
+    photos: []
+  })
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+
+  http.post('http://localhost:3030/qa/questions', questionPost, params)
+  // http.post('http://localhost:3030/qa/questions/41/answers', answerPost, params)
 
   // wait 1 second between requests
   sleep(1)
