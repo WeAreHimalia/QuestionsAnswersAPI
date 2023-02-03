5660b0f8514921cd47f6b914e432e9d246e2728b
mindiweik <mindiweik@gmail.com>
2023-02-03 13:57:25 -0700

    confirm new relic is working, restart tests and hide new relic logs

--- testing/k6/script.js
+++ testing/k6/script.js
@@ -2,9 +2,9 @@ import http from 'k6/http'
 import { check, sleep } from 'k6'
 
 export const options = {
-  vus: 65,
+  vus: 1,
   duration: '60s',
-  rps: 65,
+  rps: 1,
   thresholds: {
     http_req_failed: ['rate<0.01'], // http errors should be less than 1%
     http_req_duration: ['p(90)<200'], // 90% of requests should be below 200ms
